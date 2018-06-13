461cd7325a441330dfa56acaf5c19c59944eec49
fangdong <fangdong@yodoo.net.cn>
2018-06-13 15:33:53 +0800

    增加多版本支持以及测试代码

---
+++ src/TaxClassificationCodeCHS.TestConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace TaxClassificationCodeCHS.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var taxCode = "102";
+            var code = TaxClassificationCodeHelper.GetCode(taxCode);
+            if (code != null)
+            {
+                Console.WriteLine("名称：{0}  简称：{1} 完整编码：{2}", code.Name, code.AbbrName, code.Code);
+            }
+            else
+            {
+                Console.WriteLine("未能找到对应编码");
+            }
+            Console.WriteLine(TaxClassificationCodeHelper.GetChildren(taxCode).Count());
+            Console.ReadLine();
+        }
+    }
+}
